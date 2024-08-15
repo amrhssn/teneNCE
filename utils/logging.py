6ce008a8670f2551931165e29bac912cd04a63da
Amirhossein Nouranizadeh <anouranizadeh@interrodata.com>
2024-08-15 17:07:13 +0330

    files removed.

--- utils/logging.py
+++
@@ -1,9 +0,0 @@
-import json
-
-
-def print_dictionary(nested_dict):
-    # Convert the nested dictionary to a JSON string with indentation
-    pretty_json_str = json.dumps(nested_dict, indent=4)
-
-    # Print the formatted JSON string
-    print(pretty_json_str)
